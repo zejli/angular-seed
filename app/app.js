e0392b8068bded9d020a2b997d9fa4436c98f889
zejli <salma.zejli@insa-lyon.fr>
2015-04-11 10:11:04 +0200

    commiter le fichier app.js

--- app/app.js
+++ app/app.js
@@ -1,43 +1,12 @@
+'use strict';
 
-(function(){
-
-  var app = angular.module('graphApp',[]);
-
-  app.controller('graphController', function($scope){
-
- $scope.width = 600;
- $scope.height = 400;
- $scope.yAxis = "Sales";
- $scope.xAxis = "2014";
- $scope.data = [
-  {
-    label: 'January',
-    value: 36
-  },
-  {
-    label: 'February',
-    value: 54
-  },
-
-  // .... and so on .....
-
-  {
-    label: 'November',
-    value: 252
-  },
-  {
-    label: 'December',
-    value: 342
-  }
-];
-
-$scope.max = 0;
-
-var arrLength = $scope.data.length;
-for (var i = 0; i < arrLength; i++) {
-  // Find Maximum X Axis Value
-  if ($scope.data[i].value > $scope.max)
-  $scope.max = $scope.data[i].value;
-}
-});
-})();
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.view1',
+  'myApp.view2',
+  'myApp.version'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
